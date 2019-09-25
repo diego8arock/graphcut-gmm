92b9b256ffd160f730ddf9ed9a7cfcc2505ec8f3
Diego Alejandro Ochoa Gutierrez <diego.ochoa@globant.com>
2019-09-25 15:39:45 -0500

    Refractor Grab Cut algorithm

---
+++ GCApplication.cpp
@@ -0,0 +1,424 @@
+#include "GCApplication.h"
+#include <map>
+
+using namespace cv;
+using namespace std;
+
+static void getBinMask(const Mat& comMask, Mat& binMask)
+{
+	if (comMask.empty() || comMask.type() != CV_8UC1)
+		CV_Error(Error::StsBadArg, "comMask is empty or has incorrect type (not CV_8UC1)");
+	if (binMask.empty() || binMask.rows != comMask.rows || binMask.cols != comMask.cols)
+		binMask.create(comMask.size(), CV_8UC1);
+	binMask = comMask & 1;
+}
+
+void GCApplication::reset()
+{
+	if (!mask.empty())
+		mask.setTo(Scalar::all(GC_BGD));
+	bgdPxls.clear(); fgdPxls.clear();
+	prBgdPxls.clear();  prFgdPxls.clear();
+	isInitialized = false;
+	rectState = NOT_SET;
+	lblsState = NOT_SET;
+	prLblsState = NOT_SET;
+	iterCount = 0;
+}
+void GCApplication::setImageAndWinName(const Mat& _image, const std::string& _winName)
+{
+	if (_image.empty() || _winName.empty())
+		return;
+	image = &_image;
+	segment = Mat(image->rows, image->cols, CV_8UC3, Scalar(255,255,255));
+	winName = &_winName;
+	mask.create(image->size(), CV_8UC1);
+	reset();
+}
+void GCApplication::showImage() const
+{
+	if (image->empty() || winName->empty())
+		return;
+	Mat res;
+	Mat binMask;
+	if (!isInitialized)
+		image->copyTo(res);
+	else
+	{
+		getBinMask(mask, binMask);
+		image->copyTo(res, binMask);
+	}
+	vector<Point>::const_iterator it;
+	for (it = bgdPxls.begin(); it != bgdPxls.end(); ++it)
+		circle(res, *it, radius, BLUE, thickness);
+	for (it = fgdPxls.begin(); it != fgdPxls.end(); ++it)
+		circle(res, *it, radius, RED, thickness);
+	for (it = prBgdPxls.begin(); it != prBgdPxls.end(); ++it)
+		circle(res, *it, radius, LIGHTBLUE, thickness);
+	for (it = prFgdPxls.begin(); it != prFgdPxls.end(); ++it)
+		circle(res, *it, radius, PINK, thickness);
+	if (rectState == IN_PROCESS || rectState == SET)
+		rectangle(res, Point(rect.x, rect.y), Point(rect.x + rect.width, rect.y + rect.height), GREEN, 2);
+	imshow(*winName, res);
+}
+void GCApplication::setRectInMask()
+{
+	CV_Assert(!mask.empty());
+	mask.setTo(GC_BGD);
+	rect.x = max(0, rect.x);
+	rect.y = max(0, rect.y);
+	rect.width = min(rect.width, image->cols - rect.x);
+	rect.height = min(rect.height, image->rows - rect.y);
+	(mask(rect)).setTo(Scalar(GC_PR_FGD));
+}
+void GCApplication::setLblsInMask(int flags, Point p, bool isPr)
+{
+	vector<Point>* bpxls, * fpxls;
+	uchar bvalue, fvalue;
+	if (!isPr)
+	{
+		bpxls = &bgdPxls;
+		fpxls = &fgdPxls;
+		bvalue = GC_BGD;
+		fvalue = GC_FGD;
+	}
+	else
+	{
+		bpxls = &prBgdPxls;
+		fpxls = &prFgdPxls;
+		bvalue = GC_PR_BGD;
+		fvalue = GC_PR_FGD;
+	}
+	if (flags & BGD_KEY)
+	{
+		bpxls->push_back(p);
+		circle(mask, p, radius, bvalue, thickness);
+	}
+	if (flags & FGD_KEY)
+	{
+		fpxls->push_back(p);
+		circle(mask, p, radius, fvalue, thickness);
+	}
+}
+void GCApplication::mouseClick(int event, int x, int y, int flags, void*)
+{
+	// TODO add bad args check
+	switch (event)
+	{
+	case EVENT_LBUTTONDOWN: // set rect or GC_BGD(GC_FGD) labels
+	{
+		bool isb = (flags & BGD_KEY) != 0,
+			isf = (flags & FGD_KEY) != 0;
+		if (rectState == NOT_SET && !isb && !isf)
+		{
+			rectState = IN_PROCESS;
+			rect = Rect(x, y, 1, 1);
+		}
+		if ((isb || isf) && rectState == SET)
+			lblsState = IN_PROCESS;
+	}
+	break;
+	case EVENT_RBUTTONDOWN: // set GC_PR_BGD(GC_PR_FGD) labels
+	{
+		bool isb = (flags & BGD_KEY) != 0,
+			isf = (flags & FGD_KEY) != 0;
+		if ((isb || isf) && rectState == SET)
+			prLblsState = IN_PROCESS;
+	}
+	break;
+	case EVENT_LBUTTONUP:
+		if (rectState == IN_PROCESS)
+		{
+			rect = Rect(Point(rect.x, rect.y), Point(x, y));
+			rectState = SET;
+			setRectInMask();
+			CV_Assert(bgdPxls.empty() && fgdPxls.empty() && prBgdPxls.empty() && prFgdPxls.empty());
+			showImage();
+		}
+		if (lblsState == IN_PROCESS)
+		{
+			setLblsInMask(flags, Point(x, y), false);
+			lblsState = SET;
+			showImage();
+		}
+		break;
+	case EVENT_RBUTTONUP:
+		if (prLblsState == IN_PROCESS)
+		{
+			setLblsInMask(flags, Point(x, y), true);
+			prLblsState = SET;
+			showImage();
+		}
+		break;
+	case EVENT_MOUSEMOVE:
+		if (rectState == IN_PROCESS)
+		{
+			rect = Rect(Point(rect.x, rect.y), Point(x, y));
+			CV_Assert(bgdPxls.empty() && fgdPxls.empty() && prBgdPxls.empty() && prFgdPxls.empty());
+			showImage();
+		}
+		else if (lblsState == IN_PROCESS)
+		{
+			setLblsInMask(flags, Point(x, y), false);
+			showImage();
+		}
+		else if (prLblsState == IN_PROCESS)
+		{
+			setLblsInMask(flags, Point(x, y), true);
+			showImage();
+		}
+		break;
+	}
+}
+int GCApplication::nextIter()
+{
+	if (isInitialized)
+		grabCut(*image, mask, rect, bgdModel, fgdModel, 1);
+	else
+	{
+		if (rectState != SET)
+			return iterCount;
+		if (lblsState == SET || prLblsState == SET)
+			grabCut(*image, mask, rect, bgdModel, fgdModel, 1, GC_INIT_WITH_MASK);
+		else
+			grabCut(*image, mask, rect, bgdModel, fgdModel, 1, GC_INIT_WITH_RECT);
+		isInitialized = true;
+	}
+	iterCount++;
+	bgdPxls.clear(); fgdPxls.clear();
+	prBgdPxls.clear(); prFgdPxls.clear();
+	return iterCount;
+}
+
+struct lex_compare {
+	bool operator() (const Vec3b lhs, const Vec3b rhs) const {
+		return lhs[0] < rhs[0] || lhs[1] < rhs[1] || lhs[2] < rhs[2];
+	}
+};
+
+bool double_equals(double a, double b, double epsilon = 0.001)
+{
+	return std::abs(a - b) < epsilon;
+}
+
+void GCApplication::processSeeds() {
+
+	Mvn mvnBG, mvnFG;
+
+	Logger::Instance()->writeToLog("Processing BG Pixels");
+	mvnBG = getMvn(bgdPxls);
+
+	Logger::Instance()->writeToLog("Processing FG Pixels");
+	mvnFG = getMvn(fgdPxls);
+
+	int rows = image->rows;
+	int cols = image->cols;
+
+	int i, j;
+	int countBG = 0, countFG = 0;
+	cout << rows << endl;
+	for (i = 0; i < rows; ++i)
+	{
+		for (j = 0; j < cols; ++j)
+		{
+			Vec3b value = (*image).at<Vec3b>(i, j);
+			Eigen::VectorXd test(3);
+			test << static_cast<double>(value[B_CHANNEL]), static_cast<double>(value[G_CHANNEL]), static_cast<double>(value[R_CHANNEL]);
+			double result1 = mvnBG.pdf(test);
+			double result2 = mvnFG.pdf(test);
+			if (result1 < result2) {
+				segment.at<Vec3b>(i, j) = Vec3b(0,0,0);
+				countBG++;
+			}
+			else {
+				countFG++;
+			}
+
+		}
+
+		if( i % 100 == 0)
+			cout << i << endl;
+	}
+
+	cout << countBG << endl;
+	cout << countFG << endl;
+
+	imwrite("mod_image.png", segment);
+	imshow("MAsk", segment);
+
+}
+
+Mvn GCApplication::getMvn(vector<Point> seeds) {
+
+	char str[500];
+	Logger::Instance()->writeToLog("Calculating Mean");
+
+	set<Vec3b, lex_compare> setSeeds;
+
+	for (Point p : seeds) {
+
+		Vec3b value = (*image).at<Vec3b>(p);
+		setSeeds.insert(value);
+
+	}
+
+	strcpy_s(str, "Total different pixels: ");
+	strcat_s(str, std::to_string(setSeeds.size()).c_str());
+
+	vector<Vec3b> uniqueSeeds;
+	set< Vec3b, lex_compare>::iterator it = setSeeds.begin();
+	while (it != setSeeds.end()) {
+
+		auto v = (*it);
+		Vec3b value(v[B_CHANNEL], v[G_CHANNEL], v[R_CHANNEL]);
+		uniqueSeeds.push_back(value);
+		it++;
+
+	}
+
+	Logger::Instance()->writeToLog(str);
+
+	vector<double> mean = calculateMean(uniqueSeeds);
+
+;	strcpy_s(str, "Mean B: ");
+	strcat_s(str, std::to_string(mean[B_CHANNEL]).c_str());
+	Logger::Instance()->writeToLog(str);
+	strcpy_s(str, "Mean G: ");
+	strcat_s(str, std::to_string(mean[G_CHANNEL]).c_str());
+	Logger::Instance()->writeToLog(str);
+	strcpy_s(str, "Mean R: ");
+	strcat_s(str, std::to_string(mean[R_CHANNEL]).c_str());
+	Logger::Instance()->writeToLog(str);
+
+	Logger::Instance()->writeToLog("Calculating Sigma");
+	array<array<double, 3>, 3> sigma = calculateCovarianceMatrix(uniqueSeeds, mean);
+
+	Logger::Instance()->writeToLog("Covaraince Matrix");
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)       
+			clog << sigma[i][j] << " \n"[j == 3 - 1];
+	
+	Logger::Instance()->writeToLog("Moprhing Mean");
+	Eigen::VectorXd mean_e = Mvn::morphMean(mean);
+	
+	Logger::Instance()->writeToLog("Morphing Sigma");
+	Eigen::MatrixXd sigma_e = Mvn::morphSigma(sigma);
+	
+	Mvn mvn(mean_e, sigma_e);
+
+	return mvn;
+
+}
+
+array<array<double, 3>, 3> GCApplication::calculateCovarianceMatrix(vector<Vec3b> pixels, vector<double> mean) {
+
+	vector<double> variance = calculateVariance(pixels);
+	array<array<double, 3>, 3> mtx;
+
+	mtx[R_CHANNEL][R_CHANNEL] = variance[R_CHANNEL];
+	mtx[G_CHANNEL][G_CHANNEL] = variance[G_CHANNEL];
+	mtx[B_CHANNEL][B_CHANNEL] = variance[B_CHANNEL];
+
+	char str[500];
+	strcpy_s(str, "Variance B: ");
+	strcat_s(str, std::to_string(variance[B_CHANNEL]).c_str());
+	Logger::Instance()->writeToLog(str);
+	strcpy_s(str, "Variance G: ");
+	strcat_s(str, std::to_string(variance[G_CHANNEL]).c_str());
+	Logger::Instance()->writeToLog(str);
+	strcpy_s(str, "Variance R: ");
+	strcat_s(str, std::to_string(variance[R_CHANNEL]).c_str());
+	Logger::Instance()->writeToLog(str);
+
+	mtx[B_CHANNEL][G_CHANNEL] = calculateCovariance(pixels, B_CHANNEL, G_CHANNEL, mean[B_CHANNEL], mean[G_CHANNEL]);
+	mtx[B_CHANNEL][R_CHANNEL] = calculateCovariance(pixels, B_CHANNEL, R_CHANNEL, mean[B_CHANNEL], mean[R_CHANNEL]);
+	mtx[G_CHANNEL][R_CHANNEL] = calculateCovariance(pixels, G_CHANNEL, R_CHANNEL, mean[G_CHANNEL], mean[R_CHANNEL]);
+
+	mtx[G_CHANNEL][B_CHANNEL] = mtx[B_CHANNEL][G_CHANNEL];
+	mtx[R_CHANNEL][B_CHANNEL] = mtx[B_CHANNEL][R_CHANNEL];
+	mtx[R_CHANNEL][G_CHANNEL] = mtx[G_CHANNEL][R_CHANNEL];
+
+	return mtx;
+
+}
+
+double GCApplication::calculateCovariance(vector<Vec3b> pixels, int channel1, int channel2, double mean1, double mean2) {
+
+	double covariance = 0;
+
+	for (Vec3b p : pixels) {
+
+		Vec3b value = p;
+
+		double c1 = static_cast<double>(value[channel1]);
+		double c2 = static_cast<double>(value[channel2]);
+		covariance += (c1 - mean1) * (c2 - mean2);
+		
+	}
+
+	int size = static_cast<int>(pixels.size());
+	return covariance / (size > 1 ? size - 1 : size);
+
+}
+
+vector<double> GCApplication::calculateVariance(vector<Vec3b> pixels) {
+
+	vector<double> mean = calculateMean(pixels);
+
+	double varianceR = 0;
+	double varianceG = 0;
+	double varianceB = 0;
+
+	for (Vec3b p : pixels) {
+		
+		Vec3b value = p;
+
+		varianceR += pow(static_cast<double>(value[R_CHANNEL]) - mean[R_CHANNEL], 2);
+		varianceG += pow(static_cast<double>(value[G_CHANNEL]) - mean[G_CHANNEL], 2);
+		varianceB += pow(static_cast<double>(value[B_CHANNEL]) - mean[B_CHANNEL], 2);
+	
+	}
+
+	int size = static_cast<int>(pixels.size());
+	varianceR /= size;
+	varianceG /= size;
+	varianceB /= size;
+
+	vector<double> v(3);
+	v[R_CHANNEL] = varianceR;
+	v[G_CHANNEL] = varianceG;
+	v[B_CHANNEL] = varianceB;
+
+	return v;
+
+}
+
+vector<double> GCApplication::calculateMean(vector<Vec3b> pixels) {
+
+	double meanR = 0;
+	double meanG = 0;
+	double meanB = 0;
+
+	for (Vec3b p : pixels) {
+
+		Vec3b value = p;
+
+		meanR += static_cast<double>(value[R_CHANNEL]);
+		meanG += static_cast<double>(value[G_CHANNEL]);
+		meanB += static_cast<double>(value[B_CHANNEL]);	
+
+	}
+
+	int size = static_cast<int>(pixels.size());
+	meanR /= size;
+	meanG /= size;
+	meanB /= size;
+
+	vector<double> v(3);
+	v[R_CHANNEL] = meanR;
+	v[G_CHANNEL] = meanG;
+	v[B_CHANNEL] = meanB;
+
+	return v;
+
+}
---
+++ GCApplication.h
@@ -0,0 +1,59 @@
+#ifndef GCAPPLICATION_H
+#define GCAPPLICATION_H
+
+#include <iostream>
+#include <set>
+#include <opencv2\highgui.hpp>
+#include <opencv2\core.hpp>
+#include <opencv2\imgproc.hpp>
+#include "Logger.h"
+#include "Mvn.h"
+
+
+class GCApplication
+{
+
+public:
+	const cv::Scalar RED = cv::Scalar(0, 0, 255);
+	const cv::Scalar PINK = cv::Scalar(230, 130, 255);
+	const cv::Scalar BLUE = cv::Scalar(255, 0, 0);
+	const cv::Scalar LIGHTBLUE = cv::Scalar(255, 255, 160);
+	const cv::Scalar GREEN = cv::Scalar(0, 255, 0);
+	const int BGD_KEY = cv::EVENT_FLAG_CTRLKEY;
+	const int FGD_KEY = cv::EVENT_FLAG_SHIFTKEY;
+	const int R_CHANNEL = 2;
+	const int G_CHANNEL = 1;
+	const int B_CHANNEL = 0;
+	enum { NOT_SET = 0, IN_PROCESS = 1, SET = 2 };
+	static const int radius = 2;
+	static const int thickness = -1;
+	void reset();
+	void setImageAndWinName(const cv::Mat& _image, const std::string& _winName);
+	void showImage() const;
+	void mouseClick(int event, int x, int y, int flags, void* param);
+	int nextIter();
+	int getIterCount() const { return iterCount; }
+	std::vector<cv::Point> getFgdPxls() { return fgdPxls; }
+	Mvn getMvn(std::vector<cv::Point> seeds);
+	void processSeeds();
+
+private:
+	void setRectInMask();
+	void setLblsInMask(int flags, cv::Point p, bool isPr);
+	const std::string* winName;
+	const cv::Mat* image;
+	cv::Mat segment;
+	cv::Mat mask;
+	cv::Mat bgdModel, fgdModel;
+	uchar rectState, lblsState, prLblsState;
+	bool isInitialized;
+	cv::Rect rect;
+	std::vector<cv::Point> fgdPxls, bgdPxls, prFgdPxls, prBgdPxls;
+	int iterCount;
+	std::vector<double> calculateMean(std::vector<cv::Vec3b> pixels);
+	std::vector<double> calculateVariance(std::vector<cv::Vec3b> pixels);
+	std::array<std::array<double, 3>, 3> calculateCovarianceMatrix(std::vector<cv::Vec3b> pixels, std::vector<double> mean);
+	double calculateCovariance(std::vector<cv::Vec3b> pixels, int channel1, int channel2, double mean1, double mean2);
+};
+
+#endif
